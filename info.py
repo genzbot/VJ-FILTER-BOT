68a03285adc6c6406592ea3936810d853326fa92
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-09-30 11:53:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+QVRHqD_g2vg3N2E9')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/ipopcorn_group')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+ceUUggj4VC1hNTFl')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 't.me/+ceUUggj4VC1hNTFl') # Support Chat Link Without https:// or @
